dff4e0791e07cb7e9ca9b9e67568aba15ff14aca
quocha11 <44391225+quocha11@users.noreply.github.com>
2018-10-23 15:34:15 +0700

    Add files via upload

---
+++ tiendien.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{	int sodienmoi , sodiencu , tieuthu, tien;
+	printf("nhap chu so dien moi ");
+	scanf("%d",&sodienmoi);
+	
+	printf("nhap chu so dien cu ");
+	scanf("%d",&sodiencu);
+	tieuthu=sodienmoi - sodiencu;
+	if(tieuthu<=50)
+	{tien=tieuthu*1500;
+		printf("so tien phai tra  %d VND",tien);
+	}
+	
+	if(tieuthu>50 && tieuthu<=100)
+	{	tien=50*1500 + (tieuthu-50)*2000;
+		printf("so tien phai tra  %d VND",tien);
+	}
+	
+	else
+	{	tien=50*1500 + 100*2000 +(tieuthu-150)*2500;
+		printf("so tien phai tra  %d VND",tien);
+	}
+		
+	return 0;
+}
